2748a09a69988e5aad8fb89fde1e6ef01cfe26aa
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-09-03 11:44:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 const db = mysql.createConnection({
     host: 'sql8.freemysqlhosting.net',
@@ -104,6 +104,6 @@ app.get('/api/posts/:id', (req, res) => {
 //     console.log('table created')
 //   });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
