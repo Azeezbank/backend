483af49588733ee574ed5b27d0e5c58530b87998
Bankole Azeez Babatunde <bankoleazeezb98@gmail.com>
2024-11-02 22:17:29 +0100

    updated

--- server.js
+++ server.js
@@ -19,6 +19,9 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
     //PORT: process.env.PORT
 });
 
